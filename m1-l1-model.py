1d456dc231c8374bbf7005bad888fa019d31ebdd
John Tucker <jtucker@spotify.com>
2025-12-25 12:54:51 -0500

    m1-l1-model.py

---
+++ m1-l1-model.py
@@ -0,0 +1,14 @@
+from pprint import pprint;
+
+from langchain.chat_models import init_chat_model;
+
+
+def main():
+    model = init_chat_model(model="gpt-5-nano");
+    response = model.invoke("What's the capital of the Moon?");
+    print(response.content);
+    pprint(response.response_metadata);
+
+
+if __name__ == "__main__":
+    main()
