1af596c464dc1a588584354d21da417b8abf97ab
John Tucker <jtucker@spotify.com>
2025-12-26 18:49:11 -0500

    context

---
+++ m2-l2-context.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass;
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
+from langchain.tools import tool, ToolRuntime
+
+QUESTION = HumanMessage(content="What is my favorite color?")
+
+@dataclass
+class ColorContext:
+    favorite_color: str = "blue"
+    least_favorite_color: str = "yellow"
+
+@tool
+def get_favorite_color(runtime: ToolRuntime) -> str:
+    """
+    Get the user's favorite color.
+    """
+    return runtime.context.favorite_color
+
+@tool
+def get_least_favorite_color(runtime: ToolRuntime) -> str:
+    """
+    Get the user's least favorite color.
+    """
+    return runtime.context.least_favorite_color
+
+agent = create_agent(
+    context_schema=ColorContext,
+    model="gpt-5-nano",
+    tools=[get_favorite_color, get_least_favorite_color],
+)
+response = agent.invoke(
+    {"messages": [QUESTION]},
+    context=ColorContext(),
+)
+print(response["messages"][-1].content)
